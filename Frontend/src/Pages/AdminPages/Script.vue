1b36edb6927d0e841dcb1a06f740c9f4a74c3dee
prodevghost <ruslan.melushev2019@mail>
2021-06-22 13:25:20 -0700

    Upload frontend

---
+++ Frontend/src/Pages/AdminPages/Script.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 ref="text">
+            &lt;script src="https://arvioliidi.com/api/calculator-script"
+            key='VEC-{{ $store.state.user.identify }}'&gt;&lt;/script&gt;
+        </h1>
+        <!-- <b-btn @click="copyScript">Copay</b-btn> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        identify: ""
+    }),
+    methods: {
+        // copyScript(){
+        //  this.selectText(this.$refs.text)
+        //  document.execCommand("copy");
+        // }
+    },
+    mounted() {
+        this.identify = this.$store.state.user.identify;
+    }
+};
+</script>
---
+++ Frontend/src/services/user.service.js
@@ -0,0 +1,40 @@
+import {httpService} from './http.service';
+
+export const getValue = ({
+   name,
+   zip,
+   email,
+   phone,
+   street,
+   city,
+   buildingType,
+   builtYear,
+   sqm,
+   roomNumber,
+   plot,
+   condition,
+   status,
+   toCompany
+}) => {
+   console.log(toCompany)
+   return httpService.post('getvalue', {
+      name,
+      zip,
+      email,
+      phone,
+      street,
+      city,
+      buildingType,
+      builtYear,
+      sqm,
+      roomNumber,
+      plot,
+      condition,
+      status,
+      toCompany
+   })
+}
+
+export const getCustomUi = ({company}) => {
+   return httpService.post('get-custom-ui', {company})
+}
